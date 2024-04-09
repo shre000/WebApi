c638bb1b48f6e1741aa96d8a30f8be4427dfadd7
Shreya pandey <65069641+shre000@users.noreply.github.com>
2024-04-09 22:08:13 +0530

    Add files via upload

---
+++ Models/UserDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioAPI.Models
+{
+    public class UserDetails
+    {
+        public int Id { get; set; } 
+        public string Name { get;set; }
+        public string Email { get;set; }
+        public string Password { get;set; }
+        public string Department { get;set; }
+    }
+}
